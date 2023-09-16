2f7741f50c50b77b607c08611eb48ccb86b39259
malistov.artem <malistov.artem@russianpost.ru>
2023-09-16 17:29:34 +0300

    add first homework

--- homework001/script.js
+++ homework001/script.js
@@ -2,7 +2,7 @@ const arr1 = [1, 15, 0, -3, 17, 0, 1, 3]
 const arr2 = [1, 15, 0, -3, 17, 0, 1, 3]
 
 // Решение в императивном стиле
-function sortListImperative(numbers) { //сортировка пузцрьком
+function sortListImperative(numbers) { //сортировка пузырьком
     for (let i = 0; i < numbers.length; i++) {
         for (let j = 1; j < numbers.length; j++) {
             if (numbers[j] > numbers[j - 1]) {
