aa03105fb0b2ce5d4241c53af20033bf8dcd35a5
malistov.artem <malistov.artem@russianpost.ru>
2023-09-16 17:28:16 +0300

    add first homework

--- homework001/script.js
+++ homework001/script.js
@@ -1,4 +1,5 @@
-const arr = [1, 15, 0, -3, 17, 0, 1, 3]
+const arr1 = [1, 15, 0, -3, 17, 0, 1, 3]
+const arr2 = [1, 15, 0, -3, 17, 0, 1, 3]
 
 // Решение в императивном стиле
 function sortListImperative(numbers) { //сортировка пузцрьком
@@ -19,3 +20,6 @@ function sortListDeclarative(numbers) {
     numbers.sort((a, b) => b - a)
     return numbers
 }
+
+console.log(`Результат сортировки в императивном стиле:\n ${arr1}\n ${sortListImperative(arr1)}`)
+console.log(`Результат сортировки в декларативном стиле:\n ${arr2}\n ${sortListImperative(arr2)}`)
